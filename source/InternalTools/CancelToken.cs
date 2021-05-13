ac29b59f94dc880f4692aaaffc35e9d5679026c6
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2021-05-12 21:10:01 -0400

    Simplifyed new statements

---
+++ source/InternalTools/CancelToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartTools
+{
+    internal class CancelToken
+    {
+        public bool CancelRquested { get; private set; } = false;
+        public void Cancel() => CancelRquested = true;
+    }
+}
