f62f4b7cea0df7a6b3456fd1ebd2f1f379d4a8a4
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2021-08-13 12:49:09 -0400

    Reverse order of track task creation to give the more complex harder tracks a headstart

--- source/IO/Chart/ChartParser_Reading.cs
+++ source/IO/Chart/ChartParser_Reading.cs
@@ -116,7 +116,7 @@ namespace ChartTools.IO.Chart
         private static Instrument<TChord>? GetInstrument<TChord>(string[] lines, Func<IEnumerable<string>, Track<TChord>?> getTrack, string instrumentPartName) where TChord : Chord
         {
             Instrument<TChord> instrument = new();
-            Difficulty[] difficulties = Enum.GetValues<Difficulty>().ToArray();
+            Difficulty[] difficulties = Enum.GetValues<Difficulty>().Reverse().ToArray();
             Task<Track<TChord>?>[] tasks = difficulties.Select(d => Task.Run(() => getTrack(GetPart(lines, $"{d}{instrumentPartName}")))).ToArray();
 
             Task.WaitAll(tasks);
