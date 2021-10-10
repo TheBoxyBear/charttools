6d5cca5ecc45d6ef7b263e4d41b37726b0378f1c
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2021-10-10 01:18:14 -0400

    Remove pitch setters

--- source/TrackObjects/Instruments/Vocals/VocalsPitch.cs
+++ source/TrackObjects/Instruments/Vocals/VocalsPitch.cs
@@ -4,45 +4,11 @@ namespace ChartTools
 {
     public struct VocalsPitch : IEquatable<VocalsPitch>, IEquatable<VocalsPitches>
     {
-        VocalsPitches _pitch = VocalsPitches.C2;
-        public VocalsPitches Pitch
-        {
-            get => _pitch;
-            set => _pitch = Enum.IsDefined(value) ? value : throw CommonExceptions.GetUndefinedException(value);
-        }
+        public VocalsPitches Pitch { get; }
+        public VocalsKey Key => (VocalsKey)((int)Pitch & 0x0F);
+        public byte Octave => (byte)(((int)Pitch & 0xF0) >> 4);
 
         public VocalsPitch(VocalsPitches pitch) => Pitch = pitch;
-
-        public VocalsKey Key
-        {
-            get => (VocalsKey)((int)Pitch & 0x0F);
-            set
-            {
-                if (!Enum.IsDefined(value))
-                    throw CommonExceptions.GetUndefinedException(value);
-
-                int unadjustedOctave = UnadjustedOctave;
-
-                if (unadjustedOctave is 0x20 or 0x60 && value != VocalsKey.C)
-                    throw new ArgumentException($"Only C can be used with octave {Octave}", nameof(value));
-
-                Pitch = (VocalsPitches)(UnadjustedOctave | (int)value);
-            }
-        }
-        public byte Octave
-        {
-            get => (byte)(UnadjustedOctave >> 4);
-            set
-            {
-                if (value is < 2 or > 6)
-                    throw new ArgumentException("The octave must be in the range 2-6.", nameof(value));
-                if (value is 2 or 6 && Key != VocalsKey.C)
-                    throw new ArgumentException($"Octave {value} can only be used with C.", nameof(value));
-
-                Pitch = (VocalsPitches)((value << 4) | (int)Key);
-            }
-        }
-        private int UnadjustedOctave => (int)Pitch & 0xF0;
 
         #region Equals
         public bool Equals(VocalsPitch other) => Pitch == other.Pitch;
