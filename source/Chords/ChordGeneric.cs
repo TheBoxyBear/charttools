9ca3fa2d6ce961f88e5446ab3b14a11837cc29df
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2022-04-21 16:26:57 -0400

    Add docs and remove values to instrument identity enums

--- source/Chords/ChordGeneric.cs
+++ source/Chords/ChordGeneric.cs
@@ -8,6 +8,7 @@ namespace ChartTools
     /// </summary>
     public abstract class Chord<TNote, Tlane, TModifier> : Chord where TNote : Note<Tlane> where Tlane : struct where TModifier : Enum
     {
+        /// <inheritdoc cref="Chord.Notes"/>
         public override abstract IEnumerable<TNote> Notes { get; }
         protected abstract TModifier DefaultModifier { get; }
         public TModifier Modifier { get; set; }
