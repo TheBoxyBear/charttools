56ceda7f8a8a14116b83285b5406b7d23fa7c5a8
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2022-08-19 14:15:05 -0400

    Remove formatting from Song

---
+++ source/IO/Midi/Serialization/InstrumentSerializer.cs
@@ -0,0 +1,15 @@
+﻿using ChartTools.IO.Serializaiton;
+
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Multimedia;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartTools.IO.Midi.Serialization
+{
+    internal class InstrumentSerializer : Serializer<Instrument, MidiChunk>
+    {
+    }
+}
