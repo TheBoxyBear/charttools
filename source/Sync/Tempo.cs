2794baeee124477723260020a67132ca4246ed2d
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2022-09-29 14:16:17 -0400

    Add tempo sync

--- source/Sync/Tempo.cs
+++ source/Sync/Tempo.cs
@@ -7,18 +7,55 @@ namespace ChartTools
     /// </summary>
     public class Tempo : TrackObjectBase
     {
+        /// <inheritdoc cref="TrackObjectBase.Position" path="/summary"/>
+        /// <remarks>If <see cref="Anchor"/> is not <see langword="null"/>, only refer to the position if <see cref="PositionSynced"/> is <see langword="true"/>.</remarks>
+        public override uint Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                PositionSynced = false;
+            }
+        }
+        private uint _position;
+
         /// <summary>
         /// New tempo
         /// </summary>
         public float Value { get; set; }
+
         /// <summary>
         /// Locks the tempo to a specific time in the song independent to the sync track.
         /// </summary>
-        public TimeSpan? Anchor { get; set; }
+        public TimeSpan? Anchor
+        {
+            get => _anchor;
+            set
+            {
+                _anchor = value;
+
+                if (value is not null)
+                    PositionSynced = false;
+            }
+        }
+        private TimeSpan? _anchor;
+
+        /// <summary>
+        /// Indicates if the tick position is up to date with <see cref="Anchor"/>.
+        /// </summary>
+        /// <remarks><see langword="true"/> if the marker has no anchor.</remarks>
+        public bool PositionSynced { get; private set; } = true;
 
         /// <summary>
         /// Creates an instance of <see cref="Tempo"/>.
         /// </summary>
         public Tempo(uint position, float value) : base(position) => Value = value;
+
+        internal void SyncPosition(uint position)
+        {
+            _position = position;
+            PositionSynced = true;
+        }
     }
 }
--- source/Tools/Optimizer.cs
+++ source/Tools/Optimizer.cs
@@ -1,7 +1,5 @@
 ﻿using ChartTools.Extensions.Linq;
 
-using Melanchall.DryWetMidi.Multimedia;
-
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -20,9 +18,9 @@ namespace ChartTools.Tools
         /// Cuts short sustains that exceed the position of the next identical note.
         /// </summary>
         /// <param name="chords">Chords to cut the sustains of</param>
-        public static void CutSustains(this IEnumerable<LaneChord> chords)
+        public static void CutSustains(this IEnumerable<LaneChord> chords, bool preOrdered = false)
         {
-            foreach ((var previous, var current) in GetTrackObjectPairs(chords))
+            foreach ((var previous, var current) in GetTrackObjectPairs(chords, preOrdered))
                 foreach (var note in current.Notes)
                 {
                     var previousNote = previous!.Notes.First(n => n.Index == note.Index);
@@ -42,9 +40,9 @@ namespace ChartTools.Tools
         /// Cuts short long track objects that exceed the start of the next one.
         /// </summary>
         /// <param name="phrases">Star power phrases to cut the lengths of</param>
-        public static void CutLengths(this IEnumerable<ILongTrackObject> phrases)
+        public static void CutLengths(this IEnumerable<ILongTrackObject> phrases, bool preOrdered = false)
         {
-            foreach ((var previous, var current) in GetTrackObjectPairs(phrases))
+            foreach ((var previous, var current) in GetTrackObjectPairs(phrases, preOrdered))
                 if (LengthNeedsCut(previous!, current))
                     previous!.Length = current.Position - previous.Position;
         }
@@ -54,10 +52,10 @@ namespace ChartTools.Tools
         /// </summary>
         /// <param name="markers">Tempo markers to remove the unneeded from</param>
         /// <exception cref="InvalidOperationException"/>
-        /// <remarks>Only use if certain that no tempo marker contain an anchor, otherwise use <see cref="RemoveUneeded(ICollection{Tempo}, uint)"/></remarks>
-        public static void RemoveUneeded(this ICollection<Tempo> markers)
+        /// <remarks>Only use if certain that no tempo marker contain an anchor, otherwise use <see cref="RemoveUneeded(ICollection{Tempo}, uint, bool)"/></remarks>
+        public static void RemoveUneeded(this ICollection<Tempo> markers, bool preOrdered = false)
         {
-            foreach ((var previous, var current) in GetTrackObjectPairs(markers))
+            foreach ((var previous, var current) in GetTrackObjectPairs(markers, preOrdered))
             {
                 if (current.Anchor is not null)
                     throw new InvalidOperationException($"Collection contains tempo marker with anchor at {current.Anchor}. Use the overload with a resolution.");
@@ -66,22 +64,38 @@ namespace ChartTools.Tools
                     markers.Remove(current);
             }
         }
-        public static void RemoveUneeded(this ICollection<Tempo> markers, uint resolution)
+        public static void RemoveUneeded(this ICollection<Tempo> markers, uint resolution, bool ticksPreOrdered = false)
         {
+            var syncked = new List<Tempo>();
+            var unsynched = new List<Tempo>();
 
+            foreach (var tempo in markers)
+            {
+                var list = tempo.PositionSynced ? syncked : unsynched;
+                list.Add(tempo);
+            }
+
+            if (!ticksPreOrdered)
+                syncked.Sort((a, b) => a.Position.CompareTo(b.Position));
+
+            unsynched.Sort((a, b) => a.Anchor!.Value.CompareTo(b.Anchor!.Value));
         }
 
         /// <summary>
         /// Removes redundant time signature markers ones.
         /// </summary>
         /// <param name="signatures">Time signatures to remove the unneeded from</param>
-        public static void RemoveUnneeded(this ICollection<TimeSignature> signatures)
+        public static void RemoveUnneeded(this ICollection<TimeSignature> signatures, bool preOrdered = false)
         {
-            foreach ((var previous, var current) in GetTrackObjectPairs(signatures))
+            foreach ((var previous, var current) in GetTrackObjectPairs(signatures, preOrdered))
                 if (previous is not null && previous.Numerator == current.Numerator && previous.Denominator == current.Denominator)
                     signatures.Remove(current);
         }
 
-        private static IEnumerable<(T?, T)> GetTrackObjectPairs<T>(IEnumerable<T> source) where T : ITrackObject => source.OrderBy(p => p.Position).RelativeLoop();
+        private static IEnumerable<(T?, T)> GetTrackObjectPairs<T>(IEnumerable<T> source, bool preOrdered) where T : ITrackObject
+        {
+            var ordered = preOrdered ? source : source.OrderBy(o => o.Position);
+            return ordered.RelativeLoop();
+        }
     }
 }
