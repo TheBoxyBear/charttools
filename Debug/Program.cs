5040ef4f8e101ff081058275c066e4c4350ee86c
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2022-10-14 11:39:31 -0400

    Remove period

--- Debug/Program.cs
+++ Debug/Program.cs
@@ -23,7 +23,7 @@ namespace Debug
         static async Task Main(string[] args)
         {
             // You can use this project to perform manual tests and experiment. Pull requests that include changes to this project will be rejected.
-            // To make sure changes to this project are not included in commits, run the command go in the Debug directory in Git Bash and run "git ls-files -z | xargs -0 git update-index --assume-unchanged". This command only needs to be ran once after cloning and if a pull includes commits with the DEBUG prefix..
+            // To make sure changes to this project are not included in commits, run the command go in the Debug directory in Git Bash and run "git ls-files -z | xargs -0 git update-index --assume-unchanged". This command only needs to be ran once after cloning and if a pull includes commits with the DEBUG prefix.
             // This project targets .NET 6 with the matching debug build of ChartTools.
         }
     }
