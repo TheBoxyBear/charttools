425fa10488f352d31f0f39a6037146dc88ebe655
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2022-10-15 19:46:49 -0400

    Create track serializer

---
+++ source/IO/Midi/Serialization/TrackSerializer.cs
@@ -0,0 +1,21 @@
+﻿using DryWetFile = Melanchall.DryWetMidi.Core.MidiFile;
+
+using ChartTools.IO.Configuration.Sessions;
+using ChartTools.IO.Midi.Mapping;
+
+using System.Collections.Generic;
+
+namespace ChartTools.IO.Midi.Serialization
+{
+    internal class TrackSerializer : TrackObjectGroupSerializer<Track>
+    {
+        private readonly DryWetFile file;
+
+        public TrackSerializer(string header, Track content, DryWetFile file, WritingSession session) : base(header, content, session) => this.file = file;
+
+        protected override IEnumerable<IMidiEventMapping>[] LaunchMappers()
+        {
+
+        }
+    }
+}
