255c8c5e26c58312fdce984d80ceaafabc096ce5
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2022-11-13 18:55:19 -0500

    Fix versions

--- source/Animations/StandardInstrumentHandPosition.cs
+++ source/Animations/StandardInstrumentHandPosition.cs
@@ -1,14 +1,13 @@
-﻿namespace ChartTools.Animations
-{
-    public class StandardInstrumentHandPosition : ITrackObject
-    {
-        public uint Position { get; set; }
-        public byte Index { get; }
+﻿namespace ChartTools.Animations;
 
-        public StandardInstrumentHandPosition(uint position, byte index)
-        {
-            Position = position;
-            Index = index;
-        }
+public class StandardInstrumentHandPosition : ITrackObject
+{
+    public uint Position { get; set; }
+    public byte Index { get; }
+
+    public StandardInstrumentHandPosition(uint position, byte index)
+    {
+        Position = position;
+        Index = index;
     }
 }
