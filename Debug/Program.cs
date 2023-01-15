f478a439681145c8023cae9afecea305ec006571
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2023-01-15 10:42:27 -0500

    DEBUG Update .NET target notice

--- Debug/Program.cs
+++ Debug/Program.cs
@@ -15,21 +15,6 @@ using ChartTools.Tools;
 
 // You can use this project to perform manual tests and experiment. Pull requests that include changes to this project will be rejected.
 // To make sure changes to this project are not included in commits, run the command go in the Debug directory in Git Bash and run "git ls-files -z | xargs -0 git update-index --assume-unchanged". This command only needs to be ran once after cloning and if a pull includes commits with the DEBUG prefix.
-// This project targets .NET 6 with the matching debug build of ChartTools.
+// This project targets .NET 7 with the matching debug build of ChartTools.
 
 Console.WriteLine("Hello World");
-
-const string path = @"E:\Users\Gaming\Desktop\Moonscraper\Nothing More\CollegeHumor\Sweet Number Pi\";
-
-var song = await Song.FromFileAsync(path + "notes.chart");
-var lastSignature = song.SyncTrack.TimeSignatures.Last();
-
-Func<ITrackObject, bool> positionCheck = t => t.Position >= lastSignature.Position;
-
-foreach (var t in song.GlobalEvents.Where(e => e.EventType == EventTypeHelper.Global.RB2CHSection).Where(positionCheck)
-    .Concat(song.SyncTrack.Tempo.Where(positionCheck)))
-{
-    t.Position -= 384;
-}
-
-await song.ToFileAsync(path + "fixed.chart");
