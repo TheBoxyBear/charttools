9acee7deff3585f50ddc0d06eb1b713a98ca7887
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2023-01-22 12:45:15 -0500

    Remove unique collections

--- source/Extensions/Collections/Unique/UniqueSelfCollection.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace ChartTools.Extensions.Collections.Unique;
-
-public class UniqueSelfCollection<T> : UniqueCollection<T, T>
-{
-    public UniqueSelfCollection(IEnumerable<T>? items = null) : base(i => i, items) { }
-}
