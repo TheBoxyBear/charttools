67e1289744efed3c5e1d91fcc2511afc6c60591c
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2023-05-28 12:36:24 -0400

    Add mouth control

---
+++ source/Animations/VocalistMouthControl.cs
@@ -0,0 +1,9 @@
+﻿namespace ChartTools.Animations;
+
+public enum VocalistMouthState : byte { Open, Close }
+
+public class VocalistMouthControl : ITrackObject
+{
+    public uint Position { get; set; }
+    public VocalistMouthState State { get; set; }
+}
