0f215d68c5b883a651faada137c66e783165743f
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2023-05-28 13:08:31 -0400

    Cleanup

--- source/Events/Event.cs
+++ source/Events/Event.cs
@@ -1,6 +1,6 @@
 ﻿using ChartTools.IO.Midi.Mapping;
+
 using Melanchall.DryWetMidi.Core;
-using System;
 
 namespace ChartTools.Events;
 
@@ -21,37 +21,37 @@ public abstract class Event : TrackObjectBase, IMidiEventMapping
             if (string.IsNullOrEmpty(value))
                 throw new FormatException("Event type is empty");
 
-        if (value.Contains(' '))
-            throw new FormatException("Event types cannot contain spaces");
+            if (value.Contains(' '))
+                throw new FormatException("Event types cannot contain spaces");
 
-        _eventType = value;
+            _eventType = value;
+        }
     }
-}
 
-private string? _argument = null;
-/// <summary>
-/// Additional data to modify the outcome of the event
-/// </summary>
-/// <remarks>A lack of argument is represented as an empty string.</remarks>
-public string? Argument
-{
-    get => _argument;
-    set => _argument = value ?? string.Empty;
-}
-
-public string EventData
-{
-    get => Argument is null ? EventType : string.Join(' ', EventType, Argument);
-    set
+    private string? _argument = null;
+    /// <summary>
+    /// Additional data to modify the outcome of the event
+    /// </summary>
+    /// <remarks>A lack of argument is represented as an empty string.</remarks>
+    public string? Argument
     {
-        var split = value.Split(' ', 2, StringSplitOptions.None);
-
-        EventType = split[0];
-        Argument = split.Length > 1 ? split[1] : string.Empty;
+        get => _argument;
+        set => _argument = value ?? string.Empty;
     }
-}
 
-public bool? ToggleState => EventType.EndsWith(EventTypeHelper.Common.ToggleOn) ? true : (EventType.EndsWith(EventTypeHelper.Common.ToggleOff) ? false : null);
+    public string EventData
+    {
+        get => Argument is null ? EventType : string.Join(' ', EventType, Argument);
+        set
+        {
+            var split = value.Split(' ', 2, StringSplitOptions.None);
+
+            EventType = split[0];
+            Argument = split.Length > 1 ? split[1] : string.Empty;
+        }
+    }
+
+    public bool? ToggleState => EventType.EndsWith(EventTypeHelper.Common.ToggleOn) ? true : (EventType.EndsWith(EventTypeHelper.Common.ToggleOff) ? false : null);
 
     public Event() : base() { }
     /// <param name="position"><inheritdoc cref="TrackObject.Position"/></param>
