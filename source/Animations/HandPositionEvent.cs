4712cbd1a8bf65eefb90982369b5439de6418339
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2023-05-28 13:03:28 -0400

    Add animation constructors

--- source/Animations/HandPositionEvent.cs
+++ source/Animations/HandPositionEvent.cs
@@ -4,9 +4,5 @@ public class HandPositionEvent : AnimationEvent
 {
     public byte Index { get; }
 
-    public HandPositionEvent(uint position, byte index)
-    {
-        Position = position;
-        Index = index;
-    }
+    public HandPositionEvent(uint position, byte index) : base(position) => Index = index;
 }
--- source/Animations/VocalistMouthEvent.cs
+++ source/Animations/VocalistMouthEvent.cs
@@ -5,4 +5,6 @@ public enum VocalistMouthState : byte { Open, Close }
 public class VocalistMouthEvent : AnimationEvent
 {
     public VocalistMouthState State { get; set; }
+
+    public VocalistMouthEvent(uint position, VocalistMouthState state) : base(position) => State = state;
 }
