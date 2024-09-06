2b4fc4c28e5a984598a73ff02a08c758128ded0d
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2024-09-06 15:38:49 -0400

    Add guide comments to Docs main

--- Docs/Program.cs
+++ Docs/Program.cs
@@ -3,6 +3,10 @@
 using Docfx;
 using Docfx.Dotnet;
 
+// Run this project to build and deploy the documentation website.
+// The API reference section is defined by yaml files in the /api directory - These files are generated from XML documentation in the code and should not be manually modified! (therefore are gitignored)
+// The Articles section is defined by markdown files in the /articles directory.
+
 var dir = Environment.GetEnvironmentVariable("SiteDir");
 var config = dir + @"\docfx.json";
 
@@ -27,6 +31,10 @@ using Process cmd = new()
     }
 };
 
+// Process must be closed with Ctrl-C or will remain open in the background blocking port 8080.
+// If this happens (Windows):
+// netstat -aof | findstr :8080
+// taskkill / f / pid <PID>
 Process.Start(new ProcessStartInfo("http://localhost:8080") { UseShellExecute = true });
 
 cmd.Start();
