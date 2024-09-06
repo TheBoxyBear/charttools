7b69b292c96d9df8f6889866ffe0d7b22af98c46
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2024-09-06 15:01:29 -0400

    #75 Add docs to EnumCache

--- ChartTools/Extensions/EnumCache.cs
+++ ChartTools/Extensions/EnumCache.cs
@@ -1,9 +1,20 @@
 ﻿namespace ChartTools.Extensions;
 
+/// <summary>
+/// Holds a cache of defines values for an enum where <see cref="Enum.GetValues{TEnum}()"/> is to be called frequently.
+/// </summary>
+/// <typeparam name="T">Type of enum</typeparam>
 internal static class EnumCache<T> where T : struct, Enum
 {
+    /// <summary>
+    /// Cached values
+    /// </summary>
+    /// <remarks>Generates the cache on first call.</remarks>
     public static T[] Values => _values ??= [.. Enum.GetValues<T>()];
     private static T[]? _values;
 
+    /// <summary>
+    /// Clears the cache.
+    /// </summary>
     public static void Clear() => _values = null;
 }
