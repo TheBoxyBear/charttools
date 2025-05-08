b6b62a2d6be975655b6b860b127d3d611f557665
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2025-05-08 11:47:44 -0400

    Align event type helpers

--- ChartTools/Events/EventTypeHelper.cs
+++ ChartTools/Events/EventTypeHelper.cs
@@ -10,7 +10,7 @@ public static class EventTypeHelper
 	/// </summary>
 	public static class Common
 	{
-		public const string ToggleOn = "on";
+		public const string ToggleOn  = "on";
 		public const string ToggleOff = "off";
 	}
 
@@ -20,43 +20,43 @@ public static class EventTypeHelper
 	public static class Global
 	{
 		public const string
-			BandJump = "band_jump",
-			BassistIdle = EventTypeHeaderHelper.Global.BassistMovement + Common.ToggleOff,
-			BassistMove = EventTypeHeaderHelper.Global.BassistMovement + Common.ToggleOn,
-			DrummerIdle = EventTypeHeaderHelper.Global.DrummerMovement + Common.ToggleOff,
-			DrummerAll = EventTypeHeaderHelper.Global.DrummerMovement + "allbeat",
-			DrummerDouble = EventTypeHeaderHelper.Global.DrummerMovement + "double",
-			DrummerHalf = EventTypeHeaderHelper.Global.DrummerMovement + "half",
-			DrummerMove = EventTypeHeaderHelper.Global.DrummerMovement + Common.ToggleOn,
-			Chorus = "chorus",
+			BandJump          = "band_jump",
+			BassistIdle       = EventTypeHeaderHelper.Global.BassistMovement + Common.ToggleOff,
+			BassistMove       = EventTypeHeaderHelper.Global.BassistMovement + Common.ToggleOn,
+			DrummerIdle       = EventTypeHeaderHelper.Global.DrummerMovement + Common.ToggleOff,
+			DrummerAll        = EventTypeHeaderHelper.Global.DrummerMovement + "allbeat",
+			DrummerDouble     = EventTypeHeaderHelper.Global.DrummerMovement + "double",
+			DrummerHalf       = EventTypeHeaderHelper.Global.DrummerMovement + "half",
+			DrummerMove       = EventTypeHeaderHelper.Global.DrummerMovement + Common.ToggleOn,
+			Chorus            = "chorus",
 			CrowdLightersFast = EventTypeHeaderHelper.Global.Crowd + "lighters_fast",
-			CrowdLightersOff = EventTypeHeaderHelper.Global.Crowd + "lighters_off",
+			CrowdLightersOff  = EventTypeHeaderHelper.Global.Crowd + "lighters_off",
 			CrowdLightersSlow = EventTypeHeaderHelper.Global.Crowd + "lighters_slow",
-			CrowdHalfTempo = EventTypeHeaderHelper.Global.Crowd + "half_tempo",
-			CrowdNormalTempo = EventTypeHeaderHelper.Global.Crowd + "normal_tempo",
-			CrowdDoubleTempo = EventTypeHeaderHelper.Global.Crowd + "double_tempo",
-			End = "end",
-			GuitaristIdle = EventTypeHeaderHelper.Global.GuitaristMovement + Common.ToggleOff,
-			GuitaristMove = EventTypeHeaderHelper.Global.GuitaristMovement + Common.ToggleOn,
-			GuitaristSoloOn = EventTypeHeaderHelper.Global.GuitaristSolo + Common.ToggleOn,
-			GuitaristSoloOff = EventTypeHeaderHelper.Global.GuitaristSolo + Common.ToggleOff,
-			GuitaristWailOn = EventTypeHeaderHelper.Global.GuitaristWail + Common.ToggleOn,
-			GuitaristWailOff = EventTypeHeaderHelper.Global.GuitaristWail + Common.ToggleOff,
-			HalfTempo = "half_tempo",
-			Idle = "idle",
-			KeyboardIdle = EventTypeHeaderHelper.Global.SingerMovement + Common.ToggleOff,
-			KeyboardMove = EventTypeHeaderHelper.Global.SingerMovement + Common.ToggleOn,
-			Lighting = "lighting",
-			MusicStart = "music_start",
-			NotrmalTempo = "normal_tempo",
-			Play = "play",
-			RB2CHSection = "section",
-			RB3Section = "prc_",
-			SingerIdle = EventTypeHeaderHelper.Global.SingerMovement + Common.ToggleOff,
-			SingerMove= EventTypeHeaderHelper.Global.SingerMovement + Common.ToggleOn,
-			SyncHeadBang = EventTypeHeaderHelper.Global.Sync + "head_bang",
-			SyncWag = EventTypeHeaderHelper.Global.Sync + "wag",
-			Verse = "verse";
+			CrowdHalfTempo    = EventTypeHeaderHelper.Global.Crowd + "half_tempo",
+			CrowdNormalTempo  = EventTypeHeaderHelper.Global.Crowd + "normal_tempo",
+			CrowdDoubleTempo  = EventTypeHeaderHelper.Global.Crowd + "double_tempo",
+			End               = "end",
+			GuitaristIdle     = EventTypeHeaderHelper.Global.GuitaristMovement + Common.ToggleOff,
+			GuitaristMove     = EventTypeHeaderHelper.Global.GuitaristMovement + Common.ToggleOn,
+			GuitaristSoloOn   = EventTypeHeaderHelper.Global.GuitaristSolo + Common.ToggleOn,
+			GuitaristSoloOff  = EventTypeHeaderHelper.Global.GuitaristSolo + Common.ToggleOff,
+			GuitaristWailOn   = EventTypeHeaderHelper.Global.GuitaristWail + Common.ToggleOn,
+			GuitaristWailOff  = EventTypeHeaderHelper.Global.GuitaristWail + Common.ToggleOff,
+			HalfTempo         = "half_tempo",
+			Idle              = "idle",
+			KeyboardIdle      = EventTypeHeaderHelper.Global.SingerMovement + Common.ToggleOff,
+			KeyboardMove      = EventTypeHeaderHelper.Global.SingerMovement + Common.ToggleOn,
+			Lighting          = "lighting",
+			MusicStart        = "music_start",
+			NotrmalTempo      = "normal_tempo",
+			Play              = "play",
+			RB2CHSection      = "section",
+			RB3Section        = "prc_",
+			SingerIdle        = EventTypeHeaderHelper.Global.SingerMovement + Common.ToggleOff,
+			SingerMove        = EventTypeHeaderHelper.Global.SingerMovement + Common.ToggleOn,
+			SyncHeadBang      = EventTypeHeaderHelper.Global.Sync + "head_bang",
+			SyncWag           = EventTypeHeaderHelper.Global.Sync + "wag",
+			Verse             = "verse";
 
 		/// <summary>
 		/// Individual lyric syllable with the syllable text as an event argument
