3656383f16542797633355774e9c8aef7b4fb904
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-01-23 16:59:14 -0500

    #109 Use mappers in serializers

--- ChartTools/IO/Chart/Serializing/MetadataSerializer.cs
+++ ChartTools/IO/Chart/Serializing/MetadataSerializer.cs
@@ -1,27 +1,12 @@
 ﻿using ChartTools.Meta;
+using ChartTools.Meta.Mapping;
 
 namespace ChartTools.IO.Chart.Serializing;
 
-internal class MetadataSerializer(Metadata content) : Serializer<Metadata, string>(ChartFormatting.MetadataHeader, content)
+internal class MetadataSerializer(Metadata content)
+	: Serializer<Metadata, string>(ChartFormatting.MetadataHeader, content)
 {
 	public override IEnumerable<string> Serialize()
-	{
-		if (Content is null)
-			yield break;
-
-		//IEnumerable<(string key, string value)> props = MetadataChartKeyAttribute.GetSerializable(Content)
-		//	.Concat(MetadataChartKeyAttribute.GetSerializable(Content.Formatting))
-		//	.Concat(MetadataChartKeyAttribute.GetSerializable(Content.Charter)
-		//	.Concat(MetadataChartKeyAttribute.GetSerializable(Content.InstrumentDifficulties))
-		//	.Concat(MetadataChartKeyAttribute.GetSerializable(Content.Streams)));
-
-		//foreach ((string key, string value) in props)
-		//	yield return ChartFormatting.Line(key, value);
-
-		if (Content.Year is not null)
-			yield return ChartFormatting.Line("Year", $"\", {Content.Year}\"");
-
-		foreach (UnidentifiedMetadata data in Content.UnidentifiedData.Where(static d => d.Origin is FileType.Chart))
-			yield return ChartFormatting.Line(data.Key, data.Value);
-	}
+		=> MetadataChartMapper.Shared.GetAll().Select(static entry
+			=> ChartFormatting.Line(entry.Key.Span, $"\"{entry.Value}\""));
 }
