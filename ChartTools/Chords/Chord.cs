4947b8e60e3b3f35b8f65e8150b564892095bd0b
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-01-26 12:17:57 -0500

    #120 Add intermediate chord type

--- ChartTools/Chords/Chord.cs
+++ ChartTools/Chords/Chord.cs
@@ -15,17 +15,25 @@ public abstract class Chord(uint position) : ITrackObject
 	internal abstract IEnumerable<TrackObjectEntry> GetChartModifierData(Chord? previous, ChartWritingSession session);
 }
 
-public abstract class Chord<TNote, TLane, TModifiers> : Chord
+public abstract class Chord<TNote, TLane> : Chord
+	where TNote : struct, IDefinedLaneNote<TLane>
+	where TLane : Enum
+{
+	public override LaneNoteCollection<TNote, TLane> Notes { get; }
+
+	public Chord(uint position) : base(position)
+		=> Notes = [];
+}
+
+public abstract class Chord<TNote, TLane, TModifiers> : Chord<TNote, TLane>
 	where TNote : struct, IDefinedLaneNote<TLane>
 	where TLane : Enum
 	where TModifiers : Enum
 {
-	public override LaneNoteCollection<TNote, TLane> Notes { get; }
-
 	public TModifiers Modifiers { get; set; }
 
 	internal abstract TModifiers DefaultModifiers { get; }
 
-	public Chord(uint position) : base(position)
-		=> Notes = [];
+	public Chord(uint position)
+		: base(position) { }
 }
