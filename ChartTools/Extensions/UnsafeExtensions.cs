a3189b6c79524682d561ef857b5205bb1a1d5c30
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-01-27 10:35:30 -0500

    #122 Add Unsafe AsReadOnly

---
+++ ChartTools/Extensions/UnsafeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ChartTools.Extensions;
+
+public static class UnsafeExtensions
+{
+	public static ref readonly TAs AsReadonly<T, TAs>(in T value)
+		where T : unmanaged, Enum
+		where TAs : unmanaged
+	{
+		ref T refVal = ref Unsafe.AsRef(in value);
+		return ref Unsafe.As<T, TAs>(ref refVal);
+	}
+}
