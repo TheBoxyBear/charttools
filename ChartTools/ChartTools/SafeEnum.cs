b2765a64e28ee61f17ee421e022336884420e266
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-02-07 16:36:38 -0500

    #125 Cleanup SafeEnum

--- ChartTools/ChartTools/SafeEnum.cs
+++ ChartTools/ChartTools/SafeEnum.cs
@@ -1,4 +1,5 @@
 using ChartTools.Extensions;
+using ChartTools.Extensions.Enums;
 
 namespace ChartTools;
 
@@ -6,14 +7,15 @@ namespace ChartTools;
 /// Wrapper struct for enum values that automatically validates on set.
 /// </summary>
 /// <typeparam name="T">enum type to wrap</typeparam>
-public struct SafeEnum<T> : IEquatable<SafeEnum<T>> where T : struct, Enum
+public struct SafeEnum<T> : IEquatable<SafeEnum<T>>
+	where T : struct, Enum
 {
     /// <summary>
     /// Gets or sets the enum value with validation on set (field-backed property).
     /// </summary>
     public T Value
     {
-        get => field;
+        readonly get => field;
         set
         {
             Validator.ValidateEnum(value);
@@ -33,52 +35,48 @@ public struct SafeEnum<T> : IEquatable<SafeEnum<T>> where T : struct, Enum
     /// <summary>
     /// Implicit operator for conversion from T SafeEnum (Useful for assignments).
     /// </summary>
-    public static implicit operator SafeEnum<T>(T value) => new(value);
+    public static implicit operator SafeEnum<T>(T value)
+		=> new(value);
 
     /// <summary>
     /// Implicit operator for conversion from SafeEnum -> T (Useful for dereference/usage).
     /// </summary>
-    public static implicit operator T(SafeEnum<T> wrapper) => wrapper.Value;
+    public static implicit operator T(SafeEnum<T> wrapper)
+		=> wrapper.Value;
 
     /// <summary>
     /// Determines if two validated enums are equal or not.
     /// </summary>
-    public bool Equals(SafeEnum<T> other)
-    {
-        return Value.Equals(other.Value);
-    }
+    public readonly bool Equals(SafeEnum<T> other)
+		=> Value == other.Value;
 
     /// <summary>
     /// Determines if a generic object is of type SafeEnum and enum value is equal to the caller.
     /// </summary>
-    public override bool Equals(object? obj)
-    {
-        return obj is SafeEnum<T> other && Equals(other);
-    }
+    public override readonly bool Equals(object? obj)
+		=> obj is SafeEnum<T> other && Equals(other);
 
     /// <summary>
     /// Gets HashCode of the enum value.
     /// </summary>
-    public override int GetHashCode()
-    {
-        return Value.GetHashCode();
-    }
+    public override readonly int GetHashCode()
+		=> Value.GetHashCode();
 
     /// <summary>
     /// Equality operator for SafeEnum type.
     /// </summary>
-    public static bool operator ==(SafeEnum<T> lhs, SafeEnum<T> rhs) => lhs.Equals(rhs);
+    public static bool operator ==(SafeEnum<T> lhs, SafeEnum<T> rhs)
+		=> lhs.Equals(rhs);
 
     /// <summary>
     /// Inequality operator for SafeEnum type.
     /// </summary>
-    public static bool operator !=(SafeEnum<T> lhs, SafeEnum<T> rhs) => !lhs.Equals(rhs);
+    public static bool operator !=(SafeEnum<T> lhs, SafeEnum<T> rhs)
+		=> !lhs.Equals(rhs);
 
     /// <summary>
     /// ToString() implementation of SafeEnum.
     /// </summary>
-    public override string ToString()
-    {
-        return Value.ToString();
-    }
+    public override readonly string ToString()
+		=> Value.ToString();
 }
