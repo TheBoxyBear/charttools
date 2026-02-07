523288f67616cb9fb57e87ed4ac371a7ff28ebab
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-02-06 21:28:21 -0500

    #109 Use multi targeting for Common

--- ChartTools/ChartTools.Common/IsExternalInit.cs
+++ ChartTools/ChartTools.Common/IsExternalInit.cs
@@ -1,6 +1,10 @@
-﻿using System.ComponentModel;
+﻿#if NETSTANDARD
+using System.ComponentModel;
 
+#pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace System.Runtime.CompilerServices;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
 
 [EditorBrowsable(EditorBrowsableState.Never)]
-public record IsExternalInit;
+public record class IsExternalInit;
+#endif
