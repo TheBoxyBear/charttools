7b64efa9517505e1f6e8e940902eb01665e63f6c
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-02-07 19:48:07 -0500

    #125 Add operators to IEnumWrapper

--- ChartTools/ChartTools/Extensions/Enums/IEnumWrapper.cs
+++ ChartTools/ChartTools/Extensions/Enums/IEnumWrapper.cs
@@ -6,39 +6,18 @@ public interface IEnumWrapper<TSelf> :
 	IEquatable<TSelf>, IEquatable<TSelf?>,
 	IComparable<TSelf>,
 	IEqualityOperators<TSelf, TSelf, bool>, IEqualityOperators<TSelf, TSelf?, bool>,
-	IComparisonOperators<TSelf, TSelf, bool>,
+	IComparisonOperators<TSelf, TSelf, bool>
 	where TSelf : struct, IEnumWrapper<TSelf>
-{
-	static abstract bool operator ==(TSelf? left, TSelf right);
+{ }
 
-	static abstract bool operator ==(TSelf left, TSelf? right);
-
-	static abstract bool operator !=(TSelf? left, TSelf right);
-
-	static abstract bool operator !=(TSelf left, TSelf? right);
-}
-
-public interface IEnumWrapper<TSelf, TEnum> :
-	IEquatable<TEnum>, IEquatable<TEnum?>,
-	IComparable<TEnum>,
-	IEqualityOperators<TSelf, TEnum, bool>, IEqualityOperators<TSelf, TEnum?, bool>,
-	IComparisonOperators<TSelf, TEnum, bool>, IComparisonOperators<TSelf, TEnum?, bool>,
-	IAdditionOperators<TSelf, TEnum, TEnum>, ISubtractionOperators<TSelf, TEnum, TEnum>,
-	IBitwiseOperators<TSelf, TEnum, TEnum>, IShiftOperators<TSelf, TEnum, TEnum>,
-	where TSelf : struct, IEnumWrapper<TSelf, TEnum>
-	where TEnum : Enum
+public interface IEnumWrapper<TSelf, TEnum> : IEnumWrapper<TSelf>,
+	IBitwiseOperators<TSelf, TSelf, TEnum>, IShiftOperators<TSelf, int, TEnum>
+	where TSelf : struct, IEnumWrapper<TSelf>, IEnumWrapper<TSelf, TEnum>
+	where TEnum : struct, Enum
 {
 	TEnum Value { get; }
 
 	static abstract implicit operator TEnum(TSelf wrapper);
 
 	static abstract implicit operator TSelf(TEnum value);
-
-	static abstract bool operator ==(TSelf left, TSelf? right);
-
-	static abstract bool operator ==(TSelf? left, TSelf right);
-
-	static abstract bool operator !=(TSelf left, TSelf? right);
-
-	static abstract bool operator !=(TSelf? left, TSelf right);
 }
