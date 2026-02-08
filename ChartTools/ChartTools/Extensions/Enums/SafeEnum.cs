459abbb4ae798663e8aba5eae84062f85d180766
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-02-07 19:48:24 -0500

    #125 Implement IEnumWrapper on SafeEnum

--- ChartTools/ChartTools/Extensions/Enums/SafeEnum.cs
+++ ChartTools/ChartTools/Extensions/Enums/SafeEnum.cs
@@ -3,49 +3,31 @@ namespace ChartTools.Extensions.Enums;
 /// <summary>
 /// Wrapper struct for enum values that automatically validates on set.
 /// </summary>
-/// <typeparam name="T">enum type to wrap</typeparam>
-public record struct SafeEnum<T> : IEquatable<T>, IEquatable<T?>
+/// <typeparam name="T">Enum type to wrap</typeparam>
+public record struct SafeEnum<T> : IEnumWrapper<SafeEnum<T>>, IEnumWrapper<SafeEnum<T>, T>
 	where T : struct, Enum
 {
-    /// <summary>
-    /// Gets or sets the enum value with validation on set (field-backed property).
-    /// </summary>
-    public T Value
-    {
-        readonly get => field;
-        set
-        {
-            Validator.ValidateEnum(value);
-            field = value;
-        }
-    }
+	/// <summary>
+	/// Gets or sets the enum value with validation on set (field-backed property).
+	/// </summary>
+	public T Value
+	{
+		readonly get => field;
+		set
+		{
+			Validator.ValidateEnum(value);
+			field = value;
+		}
+	}
 
-    /// <summary>
-    /// Initializes a new instance with enum value post validation.
-    /// </summary>
-    public SafeEnum(T value)
-    {
-        Validator.ValidateEnum(value);
-        Value = value;
-    }
-
-    /// <summary>
-    /// Implicit operator for conversion from T SafeEnum (Useful for assignments).
-    /// </summary>
-    public static implicit operator SafeEnum<T>(T value)
-		=> new(value);
-
-    /// <summary>
-    /// Implicit operator for conversion from SafeEnum -> T (Useful for dereference/usage).
-    /// </summary>
-    public static implicit operator T(SafeEnum<T> wrapper)
-		=> wrapper.Value;
-
-	public readonly bool Equals(T value)
-		=> Value == value;
-
-	public readonly bool Equals(T? nullable)
-		=> nullable is T value && Value == value;
+	/// <summary>
+	/// Initializes a new instance with enum value post validation.
+	/// </summary>
+	public SafeEnum(T value)
+	{
+		Validator.ValidateEnum(value);
+		Value = value;
+	}
 
 	/// <summary>
 	/// Gets HashCode of the enum value.
@@ -53,9 +35,71 @@ public record struct SafeEnum<T> : IEquatable<T>, IEquatable<T?>
 	public override readonly int GetHashCode()
 		=> Value.GetHashCode();
 
-    /// <summary>
-    /// ToString() implementation of SafeEnum.
-    /// </summary>
-    public override readonly string ToString()
+	/// <summary>
+	/// ToString() implementation of SafeEnum.
+	/// </summary>
+	public override readonly string ToString()
 		=> Value.ToString();
+
+	public bool Equals(SafeEnum<T>? nullable)
+		=> nullable is T value && Value == value;
+
+	public bool Equals(T other)
+		=> Value == other;
+
+	public bool Equals(T? nullable)
+		=> nullable is T value && Value == value;
+
+	public int CompareTo(SafeEnum<T> other)
+		=> Value.CompareTo<T>(other.Value);
+
+	public int CompareTo(T other)
+		=> Value.CompareTo<T>(other);
+
+	public static implicit operator T(SafeEnum<T> wrapper)
+		=> wrapper.Value;
+
+	public static implicit operator SafeEnum<T>(T value)
+		=> new(value);
+
+	#region Operators
+	public static bool operator ==(SafeEnum<T> left, SafeEnum<T>? right)
+		=> right.HasValue && left == right.Value;
+
+	public static bool operator !=(SafeEnum<T> left, SafeEnum<T>? right)
+		=> right.HasValue && left == right.Value;
+
+	public static bool operator >(SafeEnum<T> left, SafeEnum<T> right)
+		=> left.Value > right.Value;
+
+	public static bool operator >=(SafeEnum<T> left, SafeEnum<T> right)
+		=> left.Value >= right.Value;
+
+	public static bool operator <(SafeEnum<T> left, SafeEnum<T> right)
+		=> left.Value < right.Value;
+
+	public static bool operator <=(SafeEnum<T> left, SafeEnum<T> right)
+		=> left.Value <= right.Value;
+
+	public static T operator ~(SafeEnum<T> value)
+		=> ~value.Value;
+
+	public static T operator <<(SafeEnum<T> value, int shiftAmount)
+		=> value.Value << shiftAmount;
+
+	public static T operator >>(SafeEnum<T> value, int shiftAmount)
+		=> value.Value >> shiftAmount;
+
+	public static T operator >>>(SafeEnum<T> value, int shiftAmount)
+		=> value.Value >>> shiftAmount;
+
+	public static T operator &(SafeEnum<T> left, SafeEnum<T> right)
+		=> left.Value & right.Value;
+
+	public static T operator |(SafeEnum<T> left, SafeEnum<T> right)
+		=> left.Value | right.Value;
+
+	public static T operator ^(SafeEnum<T> left, SafeEnum<T> right)
+		=> left.Value ^ right.Value;
+	#endregion
 }
