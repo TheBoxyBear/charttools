5d1aea8becf0f8eff20e2b96ac9eaff006640863
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-02-10 12:49:08 -0500

    #133 Replace static interface in ILaneNote

--- ChartTools/ChartTools/Notes/ILaneNote.cs
+++ ChartTools/ChartTools/Notes/ILaneNote.cs
@@ -19,7 +19,13 @@ public interface ILaneNote<TLane> : ILaneNote
 public interface IDefinedLaneNote<TLane> : ILaneNote<TLane>
 	where TLane : Enum
 {
+#if NET7_0_OR_GREATER
 	public static abstract bool OpenExclusivity { get; }
 
 	public static abstract byte MaxLanes { get; }
+#else
+	public bool OpenExclusivity { get; }
+
+	public byte MaxLanes { get; }
+#endif
 }
