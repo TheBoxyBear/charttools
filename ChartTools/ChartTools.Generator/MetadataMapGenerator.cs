d36cdf599fdfa6b8f84a0e7704eeafa98900d062
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-03-17 13:04:41 -0400

    Support override files for docs

--- ChartTools/ChartTools.Generator/MetadataMapGenerator.cs
+++ ChartTools/ChartTools.Generator/MetadataMapGenerator.cs
@@ -11,9 +11,7 @@ using System.Text;
 
 namespace ChartTools.Generator;
 
-/// <summary>
-/// Generates mapping methods between <see cref="Metadata"/> properties and their corresponding keys in different file formats, as defined by <see cref="MetadataKeyAttribute"/>
-/// </summary>
+/// <exclude />
 [Generator]
 public class MetadataMapGenerator : IIncrementalGenerator
 {
---
+++ ChartTools/Docs/apidocs/README.txt
@@ -0,0 +1,3 @@
+Use this directory to override sections of the Api Documentation with custom markdown instead of referring to the xml tag in the code. Use this to provide code examples and embed partial usage documentation. Include links to relevant Articles.
+
+See https://github.com/dotnet/docfx/blob/main/docs/tutorial/intro_overwrite_files.md
